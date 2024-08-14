b35b726f9888bc88ed8973f72d853f7f24f0c5be
Razet97 <brandon_huanca@usmp.pe>
2024-08-14 13:39:29 -0500

    Initial commit

---
+++ src/Content.js
@@ -0,0 +1,12 @@
+// src/Content.js
+import React from 'react';
+
+function Content() {
+  return (
+    <div style={{ marginLeft: '210px', padding: '10px' }}>
+      <h1>USMP2024</h1>
+    </div>
+  );
+}
+
+export default Content;
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+// src/Sidebar.js
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div style={{ width: '200px', background: '#f0f0f0', padding: '10px' }}>
+      <h2>HOLA MUNDO</h2>
+    </div>
+  );
+}
+
+export default Sidebar;
